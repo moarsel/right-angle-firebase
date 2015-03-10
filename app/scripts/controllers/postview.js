9ee02d043392a01ff81dbeee6730fd2aa5807869
Russell Hoy <rhoy@architech.ca>
2015-03-10 16:09:35 -0400

    Added initial comment functionality

--- app/scripts/controllers/postview.js
+++ app/scripts/controllers/postview.js
@@ -1,9 +1,43 @@
 (function() {
     'use strict';
 
-    angular.module('angnewsApp').controller('PostViewCtrl', ['$scope', 'Post', '$routeParams',
-        function($scope, Post, $routeParams) {
-            $scope.post = Post.get($routeParams.postId);
+    angular.module('angnewsApp').controller('PostViewCtrl', ['$scope', 'Post', '$routeParams', 'Auth',
+        function($scope, Post, $routeParams, Auth) {
+
+            Post.get($routeParams.postId).then(function(post) {
+                $scope.post = post;
+            });
+
+            $scope.comments = Post.comments($routeParams.postId);
+
+            $scope.auth = Auth.authorize();
+            Auth.getUserProfile($scope.auth.uid)
+                .then(function(user) {
+                    $scope.user = user;
+                });
+
+            $scope.signedIn = Auth.signedIn;
+
+
+            $scope.addComment = function() {
+                if (!$scope.commentText || $scope.commentText === '') {
+                    return;
+                }
+
+                var comment = {
+                    text: $scope.commentText,
+                    creator: $scope.user.name,
+                    creatorUID: $scope.user.provider_id // jshint ignore:line
+                };
+
+                $scope.comments.$add(comment);
+
+                $scope.commentText = '';
+            };
+
+            $scope.deleteComment = function(comment) {
+                $scope.comments.$remove(comment);
+            };
         }
     ]);
 
